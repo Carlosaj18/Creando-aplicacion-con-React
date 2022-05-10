98b9926d0e9ce0a16888f6dc8fb1804758719624
Charlyboy1993 <cjaramilloportilla@gmail.com>
2022-05-10 12:31:10 -0500

    Se crea la aplicacion usando React

---
+++ src/Components/Web.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Encabezado from './Encabezado';
+import Banner from './Banner';
+import Product from './Product';
+import Footer from './Footer';
+
+function Web(){
+  return (
+    <div className="container">
+      <Encabezado />
+      <Banner />
+      <Product />
+      <Footer />
+    </div>
+  )
+}
+
+export default Web;
